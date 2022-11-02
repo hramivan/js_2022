b8fb5c41441588a49fdbeadf577ce7d8112504bb
hramivan <hramivan96@gmail.com>
2022-11-02 13:37:56 +0200

    js

---
+++ js1/hw1.js
@@ -0,0 +1,44 @@
+let a = 'hello';
+console.log(a);
+let b = 'owu';
+console.log(b);
+let c = 'com';
+console.log(c);
+let d = 'ua';
+console.log(d);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+let i = 3.14;
+console.log(i);
+let j = 2.7;
+console.log(j);
+let k = 16;
+console.log(k);
+let l = true;
+console.log(l);
+let m = false;
+console.log(m);
+
+
+let firstName = 'Іван';
+console.log(firstName);
+let middleName = 'Андрійович';
+console.log(middleName);
+let lastName = 'Храмов';
+console.log(lastName);
+let  person = (`${firstName} ${middleName} ${lastName}`);
+console.log(person);
+
+let A = 100;
+console.log(typeof A);
+let B ='100';
+console.log(typeof B);
+let C = true;
+console.log(typeof C);
+
